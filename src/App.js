65719469c0df0b50c43c7226ef0b7e3a1e07f635
naren <blazeraze.naren@gmail.com>
2020-07-03 19:48:01 +0545

    cleaned react-app and initialized with apollo graphql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      id
+    }
+  }
+`;
 
 function App() {
+  const result = useQuery(ALL_AUTHORS);
+
+  if (result.loading) return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {result.data.allAuthors.map((a) => (
+          <div key={a.id}>
+            <p>{a.name}</p>
+            <p>{a.born}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+});
+
+/* 
+const query = gql`
+  query {
+    allAuthors {
+      name
+      born
+      id
+    }
+  }
+`;
+
+client.query({ query }).then((res) => console.log(res.data)); 
+*/
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +38,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
